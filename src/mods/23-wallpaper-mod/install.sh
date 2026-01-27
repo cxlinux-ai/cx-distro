9c1d07b28c40410a0fa30a9997420f2f006a6f00
Divanshu <divanshugrover2009@gmail.com>
2026-01-28 00:19:33 +0530

    refactor: Update wallpaper assets and installation script

--- src/mods/23-wallpaper-mod/install.sh
+++ src/mods/23-wallpaper-mod/install.sh
@@ -5,27 +5,27 @@ set -u                  # treat unset variable as error
 print_ok "Cleaning and reinstalling wallpaper"
 rm /usr/share/gnome-background-properties/* -rf
 rm /usr/share/backgrounds/* -rf
-mv ./Fluent-building-night.png /usr/share/backgrounds/
-mv ./Fluent-building-light.png /usr/share/backgrounds/
-cat << EOF > /usr/share/gnome-background-properties/fluent.dark.xml
+mv ./Planet-explosion-darker.png /usr/share/backgrounds/
+mv ./Planet-explosion-dark.png /usr/share/backgrounds/
+cat << EOF > /usr/share/gnome-background-properties/planet-explosion.dark.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
 <wallpapers>
 <wallpaper deleted="false">
-<name>Fluent Building Dark</name>
-<filename>/usr/share/backgrounds/Fluent-building-night.png</filename>
+<name>Planet Explosion Dark</name>
+<filename>/usr/share/backgrounds/Planet-explosion-darker.png</filename>
 <options>zoom</options>
 <shade_type>solid</shade_type>
 </wallpaper>
 </wallpapers>
 EOF
-cat << EOF > /usr/share/gnome-background-properties/fluent.light.xml
+cat << EOF > /usr/share/gnome-background-properties/planet-explosion.light.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
 <wallpapers>
 <wallpaper deleted="false">
-    <name>Fluent Building Light</name>
-    <filename>/usr/share/backgrounds/Fluent-building-light.png</filename>
+    <name>Planet Explosion Light</name>
+    <filename>/usr/share/backgrounds/Planet-explosion-dark.png</filename>
     <options>zoom</options>
     <shade_type>solid</shade_type>
 </wallpaper>
